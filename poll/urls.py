9fc1a45f4e533a0cc40559180f89fd22106c1c6f
Paul Maigutyak <pmaigutyak@gmail.com>
2018-09-17 09:27:25 +0300

    Fix urls bug.

--- poll/urls.py
+++ poll/urls.py
@@ -4,6 +4,9 @@ from django.urls import path
 from poll import views
 
 
+app_name = 'poll'
+
+
 urlpatterns = [
 
     path('latest/', views.get_latest_poll, name='latest'),
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
 
-version = '2.8'
+version = '2.9'
 url = 'https://github.com/pmaigutyak/mp-poll'
 
 
